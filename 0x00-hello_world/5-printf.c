872a014e03946d3ab3ba56c64cb21d6e563b8229
Michael Ogunduyilemi <michaelogunduyilemi@gmail.com>
2022-09-08 18:27:04 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * printf - print formatted strings to the stdout
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
